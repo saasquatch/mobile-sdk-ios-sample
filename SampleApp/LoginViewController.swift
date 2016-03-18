680b92f4b8252992de397b6c6bcbd5aa1a8089ca
Brendan Crawford <brendan@wholepunk.com>
2016-03-18 13:15:21 -0700

    added animation when editing login text fields

--- SampleApp/LoginViewController.swift
+++ SampleApp/LoginViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var emailField: UITextField!
@@ -18,8 +18,16 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "dismissKeyboard", name: UIApplicationWillResignActiveNotification, object: nil)
+        
+        let tap = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        self.view.addGestureRecognizer(tap)
+        
         loginButton.layer.cornerRadius = 5
         loginButton.clipsToBounds = false
+        
+        emailField.delegate = self
+        passwordField.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -36,6 +44,33 @@ class LoginViewController: UIViewController {
         } else {
             self.performSegueWithIdentifier("signupsegue", sender: sender)
         }
+    }
+    
+    func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        animateTextField(true)
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        animateTextField(false)
+    }
+    
+    func animateTextField(up: Bool) {
+        
+        let movementDistance = 160
+        let movementDuration = 0.3
+        
+        let movement = (up ? -movementDistance : movementDistance)
+        
+        UIView.beginAnimations("anim", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(movementDuration)
+        self.view.frame = CGRectOffset(self.view.frame, 0, CGFloat(movement))
+        UIView.commitAnimations()
+        
     }
     
 }
