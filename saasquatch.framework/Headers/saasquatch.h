b780b9d806896c0c4578e2963e9c3e3111ebd739
Brendan Crawford <brendan@wholepunk.com>
2016-03-23 09:45:25 -0700

    added framework

---
+++ saasquatch.framework/Headers/saasquatch.h
@@ -0,0 +1,17 @@
+//
+//  saasquatch.h
+//  saasquatch
+//
+//  Created by Brendan Crawford on 2016-03-03.
+//  Copyright © 2016 Brendan Crawford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for saasquatch.
+FOUNDATION_EXPORT double saasquatchVersionNumber;
+
+//! Project version string for saasquatch.
+FOUNDATION_EXPORT const unsigned char saasquatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <saasquatch/PublicHeader.h>
