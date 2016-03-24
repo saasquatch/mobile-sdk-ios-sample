cf1c862dbcade8048de628ffcf693e9fde40d44a
Brendan Crawford <brendan@wholepunk.com>
2016-03-24 15:56:52 -0700

    added saasquatch initialization

--- SampleApp/AppDelegate.swift
+++ SampleApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import saasquatch
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Saasquatch.initialize()
         
         return true
     }
