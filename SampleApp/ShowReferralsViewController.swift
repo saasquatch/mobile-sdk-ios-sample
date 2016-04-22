d78de36d366ff671305ad25b0b1ca65110711a03
Brendan Crawford <brendan@wholepunk.com>
2016-04-22 12:15:04 -0700

    updated list referrals with table view

--- SampleApp/ShowReferralsViewController.swift
+++ SampleApp/ShowReferralsViewController.swift
@@ -10,43 +10,76 @@ import Foundation
 import UIKit
 import saasquatch
 
-class ShowReferralsViewController: UIViewController {
+class ShowReferralsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     let user = User.sharedUser
     let tenant = "acunqvcfij2l4"
-    @IBOutlet var textView: UITextView!
+    @IBOutlet var referralsTable: UITableView!
+    var referralsList: NSMutableArray?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        referralsTable.dataSource = self
+        referralsTable.delegate = self
         
         // List the referrals for our user
         Saasquatch.listReferralsForTenant(tenant, withSecret: user.secret, forReferringAccountID: user.accountId, forReferringUserID: user.id, beforeDateReferralPaid: nil, beforeDateReferralEnded: nil, withReferredModerationStatus: nil, withReferrerModerationStatus: nil, withLimit: nil, withOffset: nil, completionHandler: {(userInfo: AnyObject?, error: NSError?) in
             
             if (error != nil) {
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.textView.text = error?.localizedDescription
-                    return
-                })
+                return
             }
             
-            var userData: NSData = NSData()
-            do {
-                userData = try NSJSONSerialization.dataWithJSONObject(userInfo!, options: .PrettyPrinted)
-            } catch let error as NSError {
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.textView.text = error.localizedDescription
-                    return
-                })
+            // Parse the list of referrals
+            guard let referrals: NSArray = userInfo!["referrals"] as? NSArray else {
+                return
             }
             
-            let referrals = NSString(data: userData, encoding: NSUTF8StringEncoding)
+            let referred: NSMutableArray = []
+            
+            for referral in referrals {
+                var referralString: NSString
+                
+                guard let referredUser = referral["referredUser"] as? NSDictionary,
+                    let firstName = referredUser["firstName"] as? NSString,
+                    let referredReward = referral["referredReward"] as? NSDictionary,
+                    let discountPercent = referredReward["discountPercent"] as? NSInteger else {
+                        break
+                }
+                
+                referralString = "You gave \(firstName) \(discountPercent)% off their SaaS"
+                referred.addObject(referralString)
+            }
+            
+            self.referralsList = referred
             dispatch_async(dispatch_get_main_queue(), {
-                self.textView.text = referrals as! String
+                self.referralsTable.reloadData()
             })
+            
         })
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = referralsList?.objectAtIndex(indexPath.row) as? String
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if referralsList == nil {
+            return 0
+        }
+        
+        return referralsList!.count
+    }
+    
 }
